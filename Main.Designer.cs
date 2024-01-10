2accf684a5a653c73771b23f1d9f8a5e801eeefb
sailkaz <kazimierz1sta@gmail.com>
2024-01-10 11:47:12 +0100

    pushing an app

---
+++ Main.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace PhotoViewer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadPicture = new System.Windows.Forms.Button();
+            this.btnRemovePicture = new System.Windows.Forms.Button();
+            this.pbxDisplay = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnPrint = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadPicture
+            // 
+            this.btnLoadPicture.Location = new System.Drawing.Point(46, 381);
+            this.btnLoadPicture.Name = "btnLoadPicture";
+            this.btnLoadPicture.Size = new System.Drawing.Size(127, 39);
+            this.btnLoadPicture.TabIndex = 0;
+            this.btnLoadPicture.Text = "Wybierz";
+            this.btnLoadPicture.UseVisualStyleBackColor = true;
+            this.btnLoadPicture.Click += new System.EventHandler(this.btnLoadPicture_Click);
+            // 
+            // btnRemovePicture
+            // 
+            this.btnRemovePicture.Location = new System.Drawing.Point(624, 383);
+            this.btnRemovePicture.Name = "btnRemovePicture";
+            this.btnRemovePicture.Size = new System.Drawing.Size(127, 39);
+            this.btnRemovePicture.TabIndex = 1;
+            this.btnRemovePicture.Text = "Usuń";
+            this.btnRemovePicture.UseVisualStyleBackColor = true;
+            this.btnRemovePicture.Click += new System.EventHandler(this.btnRemovePicture_Click);
+            // 
+            // pbxDisplay
+            // 
+            this.pbxDisplay.Location = new System.Drawing.Point(172, 51);
+            this.pbxDisplay.Name = "pbxDisplay";
+            this.pbxDisplay.Size = new System.Drawing.Size(444, 271);
+            this.pbxDisplay.TabIndex = 2;
+            this.pbxDisplay.TabStop = false;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(337, 383);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(115, 40);
+            this.btnPrint.TabIndex = 3;
+            this.btnPrint.Text = "Drukuj";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.pbxDisplay);
+            this.Controls.Add(this.btnRemovePicture);
+            this.Controls.Add(this.btnLoadPicture);
+            this.MaximumSize = new System.Drawing.Size(818, 497);
+            this.MinimumSize = new System.Drawing.Size(818, 497);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Przeglądarka Zdjęć";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDisplay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadPicture;
+        private System.Windows.Forms.Button btnRemovePicture;
+        private System.Windows.Forms.PictureBox pbxDisplay;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnPrint;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+
+namespace PhotoViewer
+{
+    public partial class Main : Form
+    {
+        private string _filePath;
+        public Main()
+        {
+            InitializeComponent();
+            _filePath = Properties.Settings.Default.StartPicturePath;
+            SetBtnVisibility();
+            DisplayStartPicture();
+        }
+
+        private void DisplayStartPicture()
+        {
+            if (!String.IsNullOrWhiteSpace(_filePath))
+            {
+                DisplayPicture();
+                SetBtnVisibility();
+            };
+        }
+
+        private void DisplayPicture()
+        {
+            var startPicture = Image.FromFile(_filePath);
+            pbxDisplay.Image = startPicture;
+            pbxDisplay.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void SetBtnVisibility()
+        {
+            if (String.IsNullOrWhiteSpace(_filePath))
+            {
+                btnRemovePicture.Visible = false;
+                btnPrint.Visible = false;
+            }
+
+            else
+            {
+                btnRemovePicture.Visible = true;
+                btnPrint.Visible = true;
+            }
+        }
+
+        private void btnLoadPicture_Click(object sender, EventArgs e)
+        {
+            pbxDisplay.SizeMode = PictureBoxSizeMode.Zoom;
+            openFileDialog1.ShowDialog();
+            _filePath = openFileDialog1.FileName;
+            DisplayPicture();
+            SetStartPicturePath();
+            SetBtnVisibility();
+        }
+
+        private void btnRemovePicture_Click(object sender, EventArgs e)
+        {
+            pbxDisplay.Image = null;
+            _filePath = null;
+            SetStartPicturePath();
+            SetBtnVisibility();
+        }
+
+        private void SetStartPicturePath()
+        {
+            Properties.Settings.Default.StartPicturePath = _filePath;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            PrintDocument printDocument = new PrintDocument();
+            printDocument.PrintPage += DocPrintPage;
+            printDialog.Document = printDocument;
+            if (printDialog.ShowDialog() == DialogResult.OK)
+            {
+                printDocument.Print();
+            }
+        }
+
+        private void DocPrintPage(object sender, PrintPageEventArgs e)
+        {
+            Bitmap bm = new Bitmap(pbxDisplay.Width, pbxDisplay.Height);
+            pbxDisplay.DrawToBitmap(bm, new Rectangle(0, 0, pbxDisplay.Width, pbxDisplay.Height));
+            e.Graphics.DrawImage(bm, 0, 0);
+            bm.Dispose();
+        }
+    }
+}
